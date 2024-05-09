899c3b6612980d663c518faa6155dc86dc104ce3
mahesht2025 <maheshthota2025@gmail.com>
2024-05-09 15:05:22 +0530

    finding the vowels or consonant of user entered character

---
+++ 4_vowels.py
@@ -0,0 +1,6 @@
+#finding the vowels or consonant of user entered character
+ch = input("Enter an Character :")
+if(ch=='A' or ch=='a' or ch=='E' or ch=='e'or ch=='I' or ch=='i' or ch=='O'or ch=='o' or ch=='u'or ch=='U'):
+    print(ch , "is a vowel Character")
+else:
+    print(ch , "is an Consonant")
